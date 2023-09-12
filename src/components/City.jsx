bdbc4d3109af79eeb5c7ac067c4295eb2de603b8
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-09-12 17:13:21 +0530

    searchParams- storing state in url

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -10,7 +10,12 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  const {id} = useParams();
+  const { id } = useParams();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -21,7 +26,14 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>city{id}</h1>;
+  return (
+    <>
+      <h1>city</h1>
+      <h1>
+        Position of the city is : {lat} , {lng}
+      </h1>
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -21,10 +21,14 @@ const flagemojiToPNG = (flag) => {
 
 function CityItem({ city }) {
   console.log(city);
-  const { cityName, emoji, date, id } = city;
+  const { cityName, emoji, date, id, position } = city;
+  console.log(position);
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
